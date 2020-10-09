0e1528f0da72133b2d383bfeb5ce0cb8ca1f5af1
estebang625 <estebang7511@gmail.com>
2020-10-09 11:20:53 -0700

    new commit

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,14 +5,14 @@ import './Nav.scss'
 function Nav() {
 return(  
     <nav>
-        
-        <Link to='/'>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous"></link>
+        <i class="fas fa-spider"><Link to='/'>
         Welcome Home!
         </Link>
         
         <Link to='/comicbooks'>
         Spidey Comics!
-        </Link>
+        </Link></i>
     </nav>
 )
     }
